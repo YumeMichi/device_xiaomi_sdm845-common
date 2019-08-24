23b626eca099aebbae968f386d9c9d7d49126917
YumeMichi <do4suki@gmail.com>
2019-08-25 03:19:02 +0800

    sdm845-common: Initial PE Plus bringup
    Change-Id: If0593750a48f04b70e9ebaeae656e59824226f53
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -13,8 +13,7 @@ $(call inherit-product-if-exists, vendor/xiaomi/sdm845-common/sdm845-common-vend
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
@@ -286,10 +285,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service \
     thermal.sdm845
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
