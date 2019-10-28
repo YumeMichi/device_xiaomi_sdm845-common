10acc4bce5415c5bd347892b2cbc642efd91c2f1
YumeMichi <do4suki@gmail.com>
2019-10-28 09:56:11 +0800

    sdm845-common: Add Pixel power HAL
    Change-Id: I303778ede5202349ba27d4e417fcc17a72c4314a
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -242,10 +242,10 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.xiaomi_sdm845
+    android.hardware.power@1.3-service.pixel-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/power/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # QTI
 PRODUCT_COPY_FILES += \
