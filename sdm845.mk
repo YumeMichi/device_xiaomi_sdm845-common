5600a7cfffc5fa223e887e6e2c4065d6b7308265
YumeMichi <do4suki@gmail.com>
2020-05-05 16:56:23 +0800

    sdm845-common: Ship prebuilt thermal and VR HALs
    Change-Id: Iee738a164509e09cec787533e2fe9622740ce1ff
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -272,8 +272,7 @@ PRODUCT_PACKAGES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sdm845
+    android.hardware.thermal@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
@@ -291,8 +290,7 @@ PRODUCT_PACKAGES += \
 # VR
 PRODUCT_PACKAGES += \
     android.hardware.vr@1.0-impl \
-    android.hardware.vr@1.0-service \
-    vr.sdm845
+    android.hardware.vr@1.0-service
 
 # WiFi
 PRODUCT_PACKAGES += \
