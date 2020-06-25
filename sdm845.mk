336c1c792e06ba1efea9b8ec481f8ac93a175cb7
Kra1o5 <kra1o5x@gmail.com>
2020-06-25 17:41:48 +0200

    sdm845-common: Don't define Snap camera package
    Change-Id: Ibb1c9a9f7c181b3ef7373c585d31f4c7bbeda064

--- sdm845.mk
+++ sdm845.mk
@@ -109,7 +109,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libdng_sdk.vendor \
-    Snap \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # Common init scripts
