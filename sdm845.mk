f97490ecd566138b014ba0f429d2be266b7991ff
Bharat Pawar <bpawar@codeaurora.org>
2020-07-30 20:18:30 +0530

    sdm845-common: vibrator: Enable LED based vibrator
    Change-Id: I079eefaefff77fe19304acc8749773febf91a7fc

--- sdm845.mk
+++ sdm845.mk
@@ -286,6 +286,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
+
 # VNDK
 # FIXME: master: compat for libprotobuf
 # See https://android-review.googlesource.com/c/platform/prebuilts/vndk/v28/+/1109518
