e39cec91d216a5e116dc5e100f0d7c2b5d285bce
Jyotiraditya <imjyotiraditya@pm.me>
2020-09-23 09:44:39 -0300

    sdm845-common: Sync kernel flags from CAF
    removes some flags because we've stopped using lineage way
    for inline kernel compilation and switched to CAF way
    
    Signed-off-by: Mesquita <mickaelmendes50@gmail.com>
    Change-Id: Ib2a8140fcf4b5870159149d7d0327dc8f6e44d95

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -31,15 +31,16 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA84000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true loop.max_part=7 androidboot.usbcontroller=a600000.dwc3
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_MKBOOTIMG_ARGS := --header_version 1
+TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_ARCH := arm64
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-  TARGET_KERNEL_CLANG_COMPILE := true
-  TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
-endif
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_VERSION := 4.9
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
+TARGET_USES_UNCOMPRESSED_KERNEL := false
 
 # Platform
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno630
