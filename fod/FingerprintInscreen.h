6aeb82d3b9a9533cdcbf357c65466824b173a8f5
YumeMichi <do4suki@gmail.com>
2021-03-17 16:38:11 +0800

    sdm845-common: Adapt FOD for Ruby
    Signed-off-by: YumeMichi <do4suki@gmail.com>
    Change-Id: Iaa14d86dbf53ff2479fa3499040239506185d28c

--- fod/FingerprintInscreen.h
+++ fod/FingerprintInscreen.h
@@ -14,14 +14,14 @@
  * limitations under the License.
  */
 
-#ifndef VENDOR_PA_BIOMETRICS_FINGERPRINT_INSCREEN_V1_0_FINGERPRINTINSCREEN_H
-#define VENDOR_PA_BIOMETRICS_FINGERPRINT_INSCREEN_V1_0_FINGERPRINTINSCREEN_H
+#ifndef VENDOR_AOSPA_BIOMETRICS_FINGERPRINT_INSCREEN_V1_0_FINGERPRINTINSCREEN_H
+#define VENDOR_AOSPA_BIOMETRICS_FINGERPRINT_INSCREEN_V1_0_FINGERPRINTINSCREEN_H
 
-#include <vendor/pa/biometrics/fingerprint/inscreen/1.0/IFingerprintInscreen.h>
+#include <vendor/aospa/biometrics/fingerprint/inscreen/1.0/IFingerprintInscreen.h>
 #include <vendor/xiaomi/hardware/fingerprintextension/1.0/IXiaomiFingerprint.h>
 
 namespace vendor {
-namespace pa {
+namespace aospa {
 namespace biometrics {
 namespace fingerprint {
 namespace inscreen {
@@ -61,7 +61,7 @@ class FingerprintInscreen : public IFingerprintInscreen {
 }  // namespace inscreen
 }  // namespace fingerprint
 }  // namespace biometrics
-}  // namespace pa
+}  // namespace aospa
 }  // namespace vendor
 
-#endif  // VENDOR_PA_BIOMETRICS_FINGERPRINT_INSCREEN_V1_0_FINGERPRINTINSCREEN_H
+#endif  // VENDOR_AOSPA_BIOMETRICS_FINGERPRINT_INSCREEN_V1_0_FINGERPRINTINSCREEN_H
